7b04a735c9769230778221e91137df47ae7b4a26
yaotthaha <yaotthaha@foxmail.com>
2023-06-06 22:31:56 +0800

    add workflow-go wait_time support

--- execPlugin/workflow-go/workflow-go.go
+++ execPlugin/workflow-go/workflow-go.go
@@ -3,8 +3,10 @@ package workflow_go
 import (
 	"context"
 	"fmt"
+	"github.com/yaotthaha/cdns/lib/types"
 	"net/http"
 	"sync"
+	"time"
 
 	"github.com/yaotthaha/cdns/adapter"
 	"github.com/yaotthaha/cdns/log"
@@ -27,10 +29,12 @@ type WorkflowGo struct {
 	core         adapter.ExecPluginCore
 	workflowTags []string
 	workflow     []adapter.Workflow
+	waitTime     time.Duration
 }
 
 type option struct {
-	Workflows []string `yaml:"workflows"`
+	Workflows []string           `yaml:"workflows"`
+	WaitTime  types.TimeDuration `yaml:"wait_time"`
 }
 
 func NewWorkflowGo(tag string, args map[string]any) (adapter.ExecPlugin, error) {
@@ -50,6 +54,10 @@ func NewWorkflowGo(tag string, args map[string]any) (adapter.ExecPlugin, error) 
 		return nil, fmt.Errorf("workflows must be at least 1")
 	}
 	w.workflowTags = op.Workflows
+	if op.WaitTime > 0 && len(op.Workflows) != 2 {
+		return nil, fmt.Errorf("workflows must be 2 if wait_time is set")
+	}
+	w.waitTime = time.Duration(op.WaitTime)
 	return w, nil
 }
 
@@ -98,7 +106,73 @@ type respMsg struct {
 	dnsCtx *adapter.DNSContext
 }
 
-func (w *WorkflowGo) Exec(ctx context.Context, _ map[string]any, dnsCtx *adapter.DNSContext) bool {
+func (w *WorkflowGo) exec1(ctx context.Context, _ map[string]any, dnsCtx *adapter.DNSContext) bool {
+	w.logger.DebugContext(ctx, "workflow-go start")
+	defer w.logger.DebugContext(ctx, "workflow-go end")
+	respChan := make(chan *respMsg, 1)
+	var respDNSCtx *respMsg
+	runCtx, runCancel := context.WithCancel(ctx)
+	wg := sync.WaitGroup{}
+	for index, workflow := range w.workflow {
+		itemDNSCtx := dnsCtx.Clone()
+		wg.Add(1)
+		go func(runCtx context.Context, workflow adapter.Workflow, dnsCtx *adapter.DNSContext, index int) {
+			defer wg.Done()
+			if index == 1 {
+				w.logger.DebugContext(ctx, fmt.Sprintf("workflow-go wait time start, wait %s", w.waitTime.String()))
+				select {
+				case <-runCtx.Done():
+					return
+				case <-time.After(w.waitTime):
+					w.logger.DebugContext(ctx, "workflow-go wait time end")
+				}
+			}
+			runCtx = log.AddContextTag(runCtx)
+			ctxTag := log.GetContextTag(runCtx)
+			w.logger.DebugContext(ctx, fmt.Sprintf("workflow [%s] start, id: %s", workflow.Tag(), ctxTag))
+			workflow.Exec(runCtx, dnsCtx)
+			select {
+			case <-runCtx.Done():
+				return
+			default:
+			}
+			if dnsCtx.RespMsg != nil {
+				select {
+				case respChan <- &respMsg{
+					id:     ctxTag,
+					dnsCtx: dnsCtx,
+				}:
+				default:
+				}
+			}
+		}(runCtx, workflow, itemDNSCtx, index)
+	}
+	go func() {
+		wg.Wait()
+		runCancel()
+	}()
+	select {
+	case <-runCtx.Done():
+	case <-ctx.Done():
+		runCancel()
+		wg.Wait()
+		return false
+	case dnsMsg := <-respChan:
+		respDNSCtx = dnsMsg
+	}
+	runCancel()
+	go func() {
+		wg.Wait()
+		close(respChan)
+	}()
+	if respDNSCtx != nil {
+		w.logger.DebugContext(ctx, fmt.Sprintf("has resp_msg, use id [%s]", respDNSCtx.id))
+		respDNSCtx.dnsCtx.SaveTo(dnsCtx)
+	}
+	return true
+}
+
+func (w *WorkflowGo) exec2(ctx context.Context, _ map[string]any, dnsCtx *adapter.DNSContext) bool {
 	w.logger.DebugContext(ctx, "workflow-go start")
 	defer w.logger.DebugContext(ctx, "workflow-go end")
 	respChan := make(chan *respMsg, 1)
@@ -153,4 +227,12 @@ func (w *WorkflowGo) Exec(ctx context.Context, _ map[string]any, dnsCtx *adapter
 		respDNSCtx.dnsCtx.SaveTo(dnsCtx)
 	}
 	return true
+}
+
+func (w *WorkflowGo) Exec(ctx context.Context, args map[string]any, dnsCtx *adapter.DNSContext) bool {
+	if w.waitTime > 0 {
+		return w.exec1(ctx, args, dnsCtx)
+	} else {
+		return w.exec2(ctx, args, dnsCtx)
+	}
 }
